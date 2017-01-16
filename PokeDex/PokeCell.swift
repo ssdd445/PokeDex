378e6f2d201d545907baf34b6fd1ebb5f8048416
Saud Waqar <saudwaqarmalik@gmail.com>
2017-01-16 19:13:19 +0500

    Changes Done!!!
    Signed-off-by: Saud Waqar <saudwaqarmalik@gmail.com>

---
+++ PokeDex/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  PokeDex
+//
+//  Created by Waqar Air on 1/8/17.
+//  Copyright © 2017 Saud. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+}
---
+++ PokeDex/Pokemon.swift
@@ -0,0 +1,9 @@
+//
+//  Pokemon.swift
+//  PokeDex
+//
+//  Created by Waqar Air on 1/8/17.
+//  Copyright © 2017 Saud. All rights reserved.
+//
+
+import Foundation
